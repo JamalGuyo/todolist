11319dc2427deb20f7cccef51e28c17c0a67cbe5
JamalGuyo <jamalguyo08@gmail.com>
2020-06-16 15:00:02 +0300

    add the create route

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express'),
 app = express(),
 port = process.env.PORT || 8080;
 
+// module imports
+const todoRoutes = require('./routes/todos');
+// configs
+app.use('/api/todos', todoRoutes);
+
 app.get('/', (req,res) => {
     res.json("{name: 'John Doe'}");
 });
