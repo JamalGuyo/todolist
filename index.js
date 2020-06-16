e72f3f8ed408b0b9aca4362f5d3057fca18d16b1
JamalGuyo <jamalguyo08@gmail.com>
2020-06-16 16:29:32 +0300

    initial api consuming commit

--- index.js
+++ index.js
@@ -6,9 +6,14 @@ port = process.env.PORT || 8080;
 // module imports
 const todoRoutes = require('./routes/todos');
 // configs
+app.use(express.static(__dirname+'/public'));
+app.use(express.static(__dirname+'/views'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use('/api/todos', todoRoutes);
-
+// 
+app.get('/', (req, res)=>{
+    res.sendFile('index.html');
+})
 
 app.listen(port, () => console.log(`todolist app listening on port ${port}...`));
