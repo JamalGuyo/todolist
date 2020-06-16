b9134c933baf5af928654138763ca7f8eed616e1
JamalGuyo <jamalguyo08@gmail.com>
2020-06-16 16:07:56 +0300

    refactor the api routes

--- routes/todos.js
+++ routes/todos.js
@@ -1,30 +1,15 @@
 const express = require('express'),
 router = express.Router();
-const db = require('../models');
+const db = require('../models'),
+helpers = require('../helpers/todos');
 
-router.get('/', (req,res) => {
-    db.Todo.find()
-    .then(todos => res.json(todos))
-    .catch(err => console.log(err))
-})
-router.post('/', (req,res)=>{
-    db.Todo.create(req.body)
-    .then(newTodo => res.status(201).json(newTodo))
-    .catch(err => console.log(err));
-})
-router.get('/:todoId', (req, res) => {
-    db.Todo.findById(req.params.todoId)
-    .then(todo => res.json(todo))
-    .catch(err => console.log(err));
-})
-router.put('/:todoId', (req, res) => {
-    db.Todo.findOneAndUpdate({_id: req.params.todoId}, req.body, {new: true})
-    .then(todo => res.json(todo))
-    .catch(err => console.log(err))
-})
-router.delete('/:todoId', (req, res) => {
-    db.Todo.deleteOne({_id: req.params.todoId})
-    .then(() => res.json({message: 'We deleted it'}))
-    .catch(err => console.log(err));
-})
+router.route('/')
+.get(helpers.getTodos)
+.post(helpers.createTodos)  
+
+router.route('/:todoId')
+.get(helpers.getTodo)
+.put(helpers.updateTodo)
+.delete(helpers.deleteTodo)
+
 module.exports = router;
