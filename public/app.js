2409cadaf66941782bbdfa70ba3ecbdab49bdf84
JamalGuyo <jamalguyo08@gmail.com>
2020-06-18 13:25:36 +0300

    use ajax to get todos and create a new todo

---
+++ public/app.js
@@ -0,0 +1,34 @@
+$('document').ready(() => {
+    const url = 'http://localhost:8080/api/todos';
+    $.getJSON(url)
+    .then(addTodos);
+    // handle form keypress
+    $('#todoInput').keypress((event)=>{
+        if(event.which === 13){
+            createTodo();
+        }
+    })
+});
+// add todo to the list of todos
+function addTodos(todos){
+    for(todo of todos){
+        addTodo(todo);
+    }
+}
+// add todo
+function addTodo(todo){
+    let newTodo = $(`<li class="task">${todo.name}</li>`);
+        if(todo.completed){
+            newTodo.addClass("done");
+        }
+        $('.list').append(newTodo);
+}
+// create a new todo
+function createTodo(){
+    let userInput = $('#todoInput').val();
+    $.post('/api/todos', {name: userInput})
+    .then(newTodo =>{
+        $('#todoInput').val('');
+        addTodo(newTodo)
+    });
+}
